b9046613b20fbe0c36c03e8f5fb2e19f1fde1098
Liliana Gtz <liliana.gura17@gmail.com>
2020-10-13 21:34:25 -0500

    New Javascript examples

---
+++ constantes.js
@@ -0,0 +1,7 @@
+//Constantes
+
+const nombre = 'Saida'
+console.log(nombre)
+
+nombre = '12312312'
+console.log(nombre)
---
+++ variables.js
@@ -0,0 +1,9 @@
+//Variables
+
+let edad = 30
+
+console.log(edad)
+
+edad = "Hola Testcafe"
+
+console.log(edad)
