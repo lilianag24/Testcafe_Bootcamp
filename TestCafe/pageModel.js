66161ff73bfa2b7511fe1a33e5bbde13c629df96
Liliana Gtz <liliana.gura17@gmail.com>
2020-10-19 13:33:00 -0500

    Actualización del archivo README y nueva prueba agregada a la practica3

--- TestCafe/pageModel.js
+++ TestCafe/pageModel.js
@@ -11,6 +11,7 @@ class Page {
         this.input31 = Selector('#email'); 
         this.button32 = Selector('#form_submit');
         this.text33 = Selector('#content');
+        this.errorMsg = Selector('body > h1');
 
         //Selectores para Practica 4
         this.link4 = Selector('a').withText('Checkboxes');
--- TestCafe/practica3.js
+++ TestCafe/practica3.js
@@ -19,7 +19,7 @@ test('Probar escribir un email en el formulario', async t =>{
     await t
         .typeText(page.input31, 'noemail@noemail.com')
         .expect(page.input31.value).eql('noemail@noemail.com')
-})
+});
 
 test('Probar que se envía correctamente el email', async t =>{
     await t
@@ -32,4 +32,17 @@ test('Probar que se envía correctamente el email', async t =>{
 
     await t
         .expect(page.text33.innerText).contains('sent', "La palabra sent no se muestra en el Text0", 'e-mail')
-})
+});
+
+test('Probar que no se ingresa email en el formulario', async t=>{
+    await t
+        .click(page.link3)
+
+    await t
+        .click(page.button32)
+
+    await t
+        .expect(page.errorMsg.innerText).eql('Internal Server Error');
+});
+
+
